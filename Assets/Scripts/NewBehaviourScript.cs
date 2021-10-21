1654ce2808beee068064332454c2f13ed75a167f
Benjamin Hoagland <benhoagland@gmail.com>
2021-10-21 06:07:16 -0400

    Add System.Diagnostics.EventLog
    Add System.Diagnostics.EventLog Reference Pre-Assembly-Rebuild

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,7 +7,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("test");
+        Log.Write("test");
     }
 
     // Update is called once per frame
